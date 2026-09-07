f3ca7b083add4b63f261a02c62a0d04252306fde
srdtrk <srdtrk@hotmail.com>
2026-09-07 16:32:36 +0400

    chore(solidity): retain OpenZeppelin license notice
    Signed-off-by: srdtrk <srdtrk@hotmail.com>

--- ibc-solidity/contracts/utils/TrieProof.sol
+++ ibc-solidity/contracts/utils/TrieProof.sol
@@ -1,5 +1,27 @@
 // SPDX-License-Identifier: MIT
-// This file is forked from OpenZeppelin Contracts to support exclusion proofs. 
+/*
+ * Copyright (c) 2016-2026 Zeppelin Group Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// This file is forked from OpenZeppelin Contracts to support exclusion proofs.
 // Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/641ba990cad2f7f70878e0d66be1bfbef95710e8/contracts/utils/cryptography/TrieProof.sol[this implementation from OpenZeppelin Contracts].
 
 pragma solidity ^0.8.26;
