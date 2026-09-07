3c4601bb945bf0ca5b4ef543d8ed46ad6726c2c0
srdtrk <srdtrk@hotmail.com>
2026-09-07 16:41:37 +0400

    refactor(solidity): remove unused Besu error
    Signed-off-by: srdtrk <srdtrk@hotmail.com>

--- ibc-solidity/contracts/light-clients/besu/errors/IBesuLightClientErrors.sol
+++ ibc-solidity/contracts/light-clients/besu/errors/IBesuLightClientErrors.sol
@@ -81,9 +81,6 @@ interface IBesuLightClientErrors {
     /// @param expectedValue The expected commitment value.
     /// @param actualValue The proven commitment value.
     error InvalidCommitmentValue(bytes32 expectedValue, bytes32 actualValue);
-    /// @notice A non-membership proof found an existing value.
-    /// @param actualValue The value found at the proven storage slot.
-    error ValueExists(bytes32 actualValue);
     /// @notice A different consensus state already exists at the submitted height.
     /// @param revisionHeight The conflicting revision height.
     error ConflictingConsensusState(uint64 revisionHeight);
