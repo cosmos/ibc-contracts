9a3ad9dac70e11aa30c1c0ba2390bf7350d6450f
srdtrk <srdtrk@hotmail.com>
2026-09-25 13:07:00 +0400

    imp: added event
    Signed-off-by: srdtrk <srdtrk@hotmail.com>

--- ibc-solidity/contracts/light-clients/besu/interfaces/IBesuLightClient.sol
+++ ibc-solidity/contracts/light-clients/besu/interfaces/IBesuLightClient.sol
@@ -15,6 +15,17 @@ interface IBesuLightClient is ILightClient {
         uint64 indexed revisionHeight, bytes32 trustedConsensusStateHash, bytes32 conflictingConsensusStateHash
     );
 
+    /// @notice Emitted when a newer header is submitted with a timestamp that is less than or equal to the trusted
+    /// consensus state timestamp.
+    /// @dev The client is permanently frozen when this is emitted.
+    /// @param revisionHeight The height of the newly submitted header.
+    /// @param trustedHeight The height of the trusted consensus state.
+    /// @param newTimestamp The timestamp of the newly submitted header.
+    /// @param trustedTimestamp The timestamp of the trusted consensus state.
+    event TimeNonMonotonicity(
+        uint64 indexed revisionHeight, uint64 trustedHeight, uint64 newTimestamp, uint64 trustedTimestamp
+    );
+
     /// @notice Returns the stored consensus state hash at a revision height.
     /// @dev The hash is `keccak256(abi.encode(IBesuLightClientMsgs.ConsensusState))`.
     /// Reverts with `ConsensusStateNotFound` if no consensus state is stored at the height.
